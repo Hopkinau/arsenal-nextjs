9780ac2ab381ace0bf28e5b6187575ad64a95b69
PENG HE <hopkinau@gmail.com>
2023-06-05 22:49:18 +1000

    update google anaysis

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,36 @@
-import Layout from '@/components/layout'
-import '@/styles/scss/main.scss'
-import { Fragment } from 'react'
-import Head from 'next/head'
+import Layout from '@/components/layout';
+import '@/styles/scss/main.scss';
+import { Fragment } from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
-  <Fragment>
-    <Head>
-      <link rel="shortcut icon" href="image/favicon.ico" type="image/x-icon" />
-    </Head>
-    <Layout>
-  <Component {...pageProps} />
-      </Layout> 
-       </Fragment>
-  )
+    <Fragment>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        strategy='afterInteractive'
+      />
+
+      <Script id='google-analytics-script' strategy='afterInteractive'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+         `}
+      </Script>
+      <Head>
+        <link
+          rel='shortcut icon'
+          href='image/favicon.ico'
+          type='image/x-icon'
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Fragment>
+  );
 }
